6bacee4b391f5a40d58af7c1a2e839f9128b977a
noamroy <80029502+noamroy@users.noreply.github.com>
2022-01-02 16:07:13 +0200

    start the frontend
    2.1.22

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -1,6 +1,6 @@
 //DEFINES
-const host = "https://ineighborhood.herokuapp.com";
-// const host = "http://localhost:8080";
+//const host = "https://ineighborhood.herokuapp.com";
+ const host = "http://localhost:8080";
 //GET PARAMS
 const urlSearchParams = new URLSearchParams(window.location.search);
 const params = Object.fromEntries(urlSearchParams.entries());
@@ -17,8 +17,6 @@ $(document).ready(function () {
     const pageName = document.getElementById('pageName');
     const navEventList = document.getElementById('navEventList');
     const navAddEvent = document.getElementById('navAddEvent');
-    //const navProgramList = document.getElementById('navProgramList'); ADD FOR FINAL PROJECT
-    //const navAddProgram = document.getElementById('navAddProgram'); ADD FOR FINAL PROJECT
     if (crud) {
         if (id) {
             stateOfPage = "DELETE/EDIT";
@@ -56,116 +54,84 @@ function showAllEvents() {
 async function getCurrentStatus(eventItem) {
 }
 async function createEventTable(events) {
-    const res_update = await fetch(`${host}/api/remote/update`, {
-        method: "GET",
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        }
-    });
     const tableStructue =
         '<table class="table" id="eventsTable">' +
-        '<thead class="thead-dark">' +
-        '<tr>' +
-        '<th scope="col">ID</th>' +
-        '<th scope="col">Name</th>' +
-        '<th scope="col">Location</th>' +
-        '<th scope="col">Time</th>' +
-        '<th scope="col">Current Status</th>' +
-        '</tr>' +
-        '</thead>' +
-        '<tbody>' +
-        '</tbody>' +
+            '<thead class="thead-dark">' +
+                '<tr>' +
+                    //'<th scope="col">ID</th>' +
+                    '<th scope="col">Name</th>' +
+                    '<th scope="col">Location</th>' +
+                    '<th scope="col">Time</th>' +
+                    '<th scope="col">Current Status</th>' +
+                    '<th scope="col">Actions</th>' +
+                '</tr>' +
+            '</thead>' +
+            '<tbody>' +
+            '</tbody>' +
         '</table>';
     $('#mainsectionflex').empty().append(tableStructue);
-    $('#eventsTable').append(events);
+    //$('#eventsTable').append(events);
     events.forEach(async s => {
         const current_status = await getCurrentStatus(s);
+        var date = new Date(s.time).toLocaleDateString();
+        var time = new Date(s.time).toLocaleTimeString();
         $("table tbody").append(
             '<tr>' +
-            '<th scope="row">' + s.id + '</th>' +
+            //'<th scope="row">' + s.id + '</th>' +
             '<td>' + s.name + '</td>' +
             '<td><p class="clickAbleP" onclick="setMap(\''+s.location+'\')">' + s.location + '</p></td>' +
-            '<td>' + s.time + '</td>' +
+            '<td>' + date +' '+ time + '</td>' +
             '<td>' + current_status + '</td>' +
             '<td>' + '<a href="home.html?crud=edit&id=' + s.id + '"><span  class="btn btn-info editbtnclass" id="editbtnid-' + s.id + '" h>Edit/Delete</span></a></td>' +
             '</tr>'
         );
     });
 }
-//~~~~~~~system form~~~~~~~~~~~~~~~~~
-function showSystemForm(systemId) {
-    createSystemForm();
-    if (systemId == -1) {
+//~~~~~~~event form~~~~~~~~~~~~~~~~~
+function showEventForm(eventId) {
+    createEventForm();
+    if (eventId == -1) {
         stateOfPage = "ADD";
         setFormForAdd();
     }
     else {
         stateOfPage = "DELETE/EDIT";
-        setFormForEditDelete(systemId);
+        setFormForEditDelete(eventId);
     }
-
 }
-async function createSystemForm(systemId) {
+async function createEventForm() {
     const formStructure =
-        '<form class="formclass" id="systemForm">' +
-        '<div class="form-outline mb-4">' +
-        '<label class="form-label" for="form6Example3">Name:</label>' +
-        '<input type="text" id="name" name="name" class="form-control" required/>' +
-        '</div>' +
-        '<div class="form-outline mb-4">' +
-        '<label class="form-label" for="form6Example4">Address:</label>' +
-        '<input type="text" id="address" name="address" class="form-control" required/>' +
-        '</div>' +
-        '<div class="form-outline mb-4">' +
-        '<label class="form-label" for="form6Example5">IP:</label>' +
-        '<input type="text" id="ip" name="ip" class="form-control" required/>' +
-        '</div>' +
-        '<div class="col-12">' +
-        '<label class="visually-hidden" for="inlineFormSelectPref">Program:</label>' +
-        '<select class="select" id="program" name="program">' +
-        '</select>' +
-        '</div>' +
-        '<div class="col-12">' +
-        '<label class="visually-hidden" for="inlineFormSelectPref">Mode:</label>' +
-        '<br />' +
-        '<select class="select" id="mode" name="mode">' +
-        '<option value="automate">Automate</option>' +
-        '<option value="manual-on">Manual On</option>' +
-        '<option value="manual-off">Manual Off</option>' +
-        '</select>' +
-        '<div class="col-12">' +
-        '<label class="visually-hidden" for="inlineFormSelectPref">Type</label>' +
-        '<br/>' +
-        '<select class="select" id="type" name="type">' +
-        '<option value="trafficLights">Traffic light</option>' +
-        '<option value="streetLights">Headlight</option>' +
-        '</select>' +
-        '</div>' +
-        '<div id="button place">' +
-        '</div>' +
+        '<form class="formclass" id="eventForm">' +
+            '<div class="form-outline mb-4">' +
+                '<label class="form-label" for="form6Example3">Name:</label>' +
+                '<input type="text" id="name" name="name" class="form-control" required/>' +
+            '</div>' +
+            '<div class="form-outline mb-4">' +
+                '<label class="form-label" for="form6Example4">Location:</label>' +
+                '<input type="text" id="location" name="location" class="form-control" required/>' +
+            '</div>' +
+            '<div class="form-outline mb-4">' +
+                '<label class="form-label" for="form6Example5">Time:</label>' +
+                '<input type="datetime-local" id="time" name="time" class="form-control" required/>' +
+            '</div>' +
+            '<div class="col-12">' +
+                '<label class="visually-hidden" for="inlineFormSelectPref">Description:</label>' +
+                '<textarea id="description" name="description"></textarea>' +
+            '</div>' +
+            '<div class="col-12">' +
+                '<label class="visually-hidden" for="inlineFormSelectPref">Police instructions:</label>' +
+                '<textarea id="government" name="government"></textarea>' +
+            '</div>' +
+            '<div class="form-outline mb-4">' +
+                '<label class="form-label" for="form6Example4">Status:</label>' +
+                '<input type="text" id="status" name="status" class="form-control" readonly/>' +
+            '</div>' +
+            '<div id="button place">' +
+            '</div>' +
         '</form>';
     $('#mainsectionflex').empty().append(formStructure);
-    setValuesForProgram();
 }
-async function setValuesForProgram() {
-    const res_Get_all_Programs = await fetch(`${host}/api/program`, {
-        method: "GET",
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        }
-    });
-    const resjson = await res_Get_all_Programs.json();
-    const program_select = document.getElementById('program');
-    resjson.forEach(element => {
-        var program_item = document.createElement('option');
-        program_item.value = element.id;
-        program_item.innerHTML = `${element.id}:${element.name}`;
-        program_select.appendChild(program_item);
-    });
-}
-// system add buttons
+// event add buttons
 async function setFormForAdd() {
     document.getElementById("button place").innerHTML = '';
     var submitButton = document.createElement('button');
@@ -176,27 +142,45 @@ async function setFormForAdd() {
     document.getElementById("button place").append(submitButton);
     submitForm();
 }
-// system edit/delete buttons
-async function setFormForEditDelete(systemId) {
-    const res_Check_If_System_Exists = await fetch(`${host}/api/neighborhoodsystem/${systemId}`, {
+// event edit/delete buttons
+async function setFormForEditDelete(eventId) {
+    const res_Check_If_Event_Exists = await fetch(`${host}/api/event/${eventId}`, {
         method: "GET",
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
         }
     });
-    const system_Resjson = await res_Check_If_System_Exists.json();
-    if (!(res_Check_If_System_Exists.status == 200)) {
-        alert("System retriving Error going. Going back to add mode");
+    const event_Resjson = await res_Check_If_Event_Exists.json();
+    if (!(res_Check_If_Event_Exists.status == 200)) {
+        alert("Event retriving Error going. Going back to add mode");
         window.location.href = 'home.html';
     }
     else {
-        document.getElementById("name").value = system_Resjson.name;
-        document.getElementById("address").value = system_Resjson.address;
-        document.getElementById("program").value = system_Resjson.program;
-        document.getElementById("ip").value = system_Resjson.ip;
-        document.getElementById("mode").value = system_Resjson.mode;
-        document.getElementById("type").value = system_Resjson.type;
+        const time=new Date(event_Resjson.time);
+        var month = time.getMonth();
+        if (month<10){
+            month = "0"+month;
+        }
+        var date = time.getDate();
+        if (date<10){
+            date = "0"+date;
+        }
+        var hours = time.getHours();
+        if (hours<10){
+            hours = "0"+hours;
+        }
+        var minutes = time.getMinutes();
+        if (minutes<10){
+            minutes = "0"+minutes;
+        }
+        const timeString= ""+time.getFullYear()+"-"+month+"-"+date+"T"+hours+":"+minutes;
+        document.getElementById("name").value = event_Resjson.name;
+        document.getElementById("location").value = event_Resjson.location;
+        document.getElementById("time").value = timeString;
+        document.getElementById("description").value = event_Resjson.description;
+        document.getElementById("government").value = event_Resjson.government;
+        document.getElementById("status").value = event_Resjson.status;
         document.getElementById("button place").innerHTML = '';
         var submitButton = document.createElement('button');
         submitButton.type = "submit";
@@ -211,14 +195,14 @@ async function setFormForEditDelete(systemId) {
         document.getElementById("button place").append(submitButton);
         document.getElementById("button place").append(deleteButton);
     }
-    submitForm(systemId);
-    deleteItem(systemId);
+    submitForm(eventId);
+    deleteItem(eventId);
 }
-//if press on delete system
-async function deleteItem(systemId) {
+//if press on delete event
+async function deleteItem(eventId) {
     var deleteButton = document.getElementById("deleteButton");
     deleteButton.addEventListener("click", async function () {
-        const res = await fetch(`${host}/api/neighborhoodsystem/${systemId}`, {
+        const res = await fetch(`${host}/api/event/${eventId}`, {
             method: "DELETE"
         })
         const resjson = await res.json();
@@ -228,53 +212,28 @@ async function deleteItem(systemId) {
         window.location.href = "home.html";
     });
 }
-//if press on add/update system
-async function submitForm(systemId) {
-    const formObj = document.getElementById("systemForm");
+//if press on add/update event
+async function submitForm(eventId) {
+    const formObj = document.getElementById("eventForm");
     formObj.addEventListener("submit", async function (event) {
         // stop form submission
         event.preventDefault();
         // validate the form
         const nameValid = formObj.elements["name"].value;
         if (!(/^[A-Za-z0-9\s]+$/.test(nameValid))) {
-            alert("You have entered an invalid system name! you can use only chars and numbers");
-            return false;
-        }
-        const addressValid = formObj.elements["address"].value;
-        const ipformat = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
-        const ipValid = formObj.elements["ip"].value;
-        if (!(ipformat.test(ipValid))) {
-            alert("You have entered an invalid IP address!");
-            return false;
-        }
-        const modeValid = formObj.elements["mode"].value;
-        const typeValid = formObj.elements["type"].value;
-        const programValid = formObj.elements["program"].value;
-        const res_Check_If_Program_Exists = await fetch(`${host}/api/program/${programValid}`, {
-            method: "GET",
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            }
-        })
-        if (res_Check_If_Program_Exists == 404) {
-            alert("Program ID is invalid, It does not exist!");
-            return false;
-        }
-        if (res_Check_If_Program_Exists == 400 || res_Check_If_Program_Exists == 401) {
-            alert("Program ID is invalid!");
+            alert("You have entered an invalid event name! you can use only chars and numbers");
             return false;
         }
         const formvalue = {
             name: nameValid,
-            address: addressValid,
-            ip: ipValid,
-            mode: modeValid,
-            type: typeValid,
-            program: Number(programValid),
+            location: formObj.elements["name"].value,
+            time: formObj.elements["time"].value,
+            description: formObj.elements["description"].value,
+            government: formObj.elements["government"].value,
+            status: "waiting for approval",
         }
         const stringBody = JSON.stringify(formvalue);
-        const host_To_Send = (stateOfPage == "ADD") ? `${host}/api/neighborhoodsystem` : `${host}/api/neighborhoodsystem/${systemId}`;
+        const host_To_Send = (stateOfPage == "ADD") ? `${host}/api/event` : `${host}/api/event/${eventId}`;
         const method_Of_Operation = (stateOfPage == "ADD") ? "POST" : "PUT";
         const res = await fetch(host_To_Send, {
             method: method_Of_Operation,
